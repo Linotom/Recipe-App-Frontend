e2b8df7d2effac81639ad6ad328dcf91e32dfd09
lino tom <linotom04@gmail.com>
2024-06-28 23:44:19 +0530

    Search Recipe Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
+import SearchRecipe from './components/SearchRecipe';
 
 function App() {
   return (
     <div>
      
     <AddRecipe/>
+    <SearchRecipe/>
     </div>
   );
 }
